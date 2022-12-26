de543186866c396ededf9192d6a0935a0aa8c423
Batrach <63921506+Batrach@users.noreply.github.com>
2022-12-26 03:49:01 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'abakumchik89@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Gendalf89'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'ababakumkum@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'KumKumAbaAba89'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('abakumchik89@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('ababakumkum@mail.ru');     // Кому будет уходить письмо 
+$mail->setFrom('ababakumkum@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('abakumchik89@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
